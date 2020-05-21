3efec19df7a90b20c6cf24cca348c982b005a9cb
Semy Levy <semylb@gmail.com>
2020-05-20 20:50:57 -0500

    Add datasets, transformation script and create query

---
+++ database/create.cypher
@@ -0,0 +1,24 @@
+CREATE CONSTRAINT ON (movie:Movie) ASSERT movie.id IS UNIQUE;
+CREATE CONSTRAINT ON (genre:Genre) ASSERT genre.id IS UNIQUE;
+CREATE CONSTRAINT ON (person:Person) ASSERT person.id IS UNIQUE;
+CREATE CONSTRAINT ON (keyword:Keyword) ASSERT keyword.id IS UNIQUE;
+
+LOAD CSV WITH HEADERS FROM "file:///genres.csv" AS row
+MERGE (m:Movie {id: toInteger(row.movie_id), title:row.movie_title})
+MERGE (g:Genre {id: toInteger(row.genre_id), name:row.genre_name})
+MERGE (m)-[:IN_GENRE]->(g);
+
+LOAD CSV WITH HEADERS FROM "file:///director.csv" AS row
+MERGE (m:Movie {id: toInteger(row.movie_id), title:row.movie_title})
+MERGE (p:Person {id: toInteger(row.director_id), name:row.director_name})
+MERGE (p)-[:DIRECTED]->(m);
+
+LOAD CSV WITH HEADERS FROM "file:///actor.csv" AS row
+MERGE (m:Movie {id: toInteger(row.movie_id), title:row.movie_title})
+MERGE (p:Person {id: toInteger(row.actor_id), name:row.actor_name})
+MERGE (p)-[:ACTED_IN]->(m);
+
+LOAD CSV WITH HEADERS FROM "file:///keyword.csv" AS row
+MERGE (m:Movie {id: toInteger(row.movie_id), title:row.movie_title})
+MERGE (k:Keyword {id: toInteger(row.keyword_id), name:row.keyword_name})
+MERGE (m)-[:HAS_KEYWORD]->(k);
---
+++ database/transform.py
@@ -0,0 +1,122 @@
+import csv
+
+from ast import literal_eval
+
+
+def readCsvFile(filename):
+    # initializing the titles and rows list
+    rows = []
+
+    # reading csv file
+    with open(filename, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.DictReader(csvfile)
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+
+    return rows
+
+
+def writeToFile(filename, fields, rows):
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+
+
+def columnsToObject(rows, fields):
+    for row in rows:
+        for field in fields:
+            row[field] = literal_eval(row[field])
+
+
+def genGenreRelations(rows):
+    res = []
+    fields = ['movie_id', 'movie_title', 'genre_id', 'genre_name']
+    for row in rows:
+        movie_title = row['title']
+        movie_id = row['id']
+        for genre in row['genres']:
+            entry = {
+                'movie_id': movie_id,
+                'movie_title': movie_title,
+                'genre_id': genre['id'],
+                'genre_name': genre['name']
+            }
+            res.append(entry)
+    return fields, res
+
+
+def genKeywordRelations(rows):
+    res = []
+    fields = ['movie_id', 'movie_title', 'keyword_id', 'keyword_name']
+    for row in rows:
+        movie_title = row['title']
+        movie_id = row['id']
+        for i, keyword in enumerate(row['keywords']):
+            if i > 4: break
+            entry = {
+                'movie_id': movie_id,
+                'movie_title': movie_title,
+                'keyword_id': keyword['id'],
+                'keyword_name': keyword['name']
+            }
+            res.append(entry)
+    return fields, res
+
+
+def genDirectorRelations(rows):
+    res = []
+    fields = ['movie_id', 'movie_title', 'director_id', 'director_name']
+    for row in rows:
+        movie_title = row['title']
+        movie_id = row['movie_id']
+        for crew in row['crew']:
+            if crew['job'] == 'Director':
+                entry = {
+                    'movie_id': movie_id,
+                    'movie_title': movie_title,
+                    'director_id': crew['id'],
+                    'director_name': crew['name']
+                }
+                res.append(entry)
+    return fields, res
+
+
+def genActorRelations(rows):
+    res = []
+    fields = ['movie_id', 'movie_title', 'actor_id', 'actor_name']
+    for row in rows:
+        movie_title = row['title']
+        movie_id = row['movie_id']
+        # Add up to 5 actors per movie
+        for i, cast in enumerate(row['cast']):
+            if i > 4: break
+            entry = {
+                'movie_id': movie_id,
+                'movie_title': movie_title,
+                'actor_id': cast['id'],
+                'actor_name': cast['name']
+            }
+            res.append(entry)
+    return fields, res
+
+
+def main():
+    rows_d = readCsvFile("dataset/tmdb_5000_movies.csv")
+    rows_c = readCsvFile("dataset/tmdb_5000_credits.csv")
+    columnsToObject(rows_d, ['genres', 'keywords'])
+    columnsToObject(rows_c, ['cast', 'crew'])
+    genres_f, genres_r = genGenreRelations(rows_d)
+    writeToFile('gen/genres.csv', genres_f, genres_r)
+    keyword_f, keyword_r = genKeywordRelations(rows_d)
+    writeToFile('gen/keyword.csv', keyword_f, keyword_r)
+    director_f, director_r = genDirectorRelations(rows_c)
+    writeToFile('gen/director.csv', director_f, director_r)
+    actor_f, actor_r = genActorRelations(rows_c)
+    writeToFile('gen/actor.csv', actor_f, actor_r)
+
+
+main()
